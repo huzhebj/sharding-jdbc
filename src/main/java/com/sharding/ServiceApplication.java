e44674861ca7fdb9815fa53e1a3dc8136d4b53fb
huzhe <huzhe@hulala.com>
2019-03-13 15:50:15 +0800

    update

--- src/main/java/com/sharding/ServiceApplication.java
+++ src/main/java/com/sharding/ServiceApplication.java
@@ -1,13 +1,11 @@
 package com.sharding;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.sharding.mapper")
 public class ServiceApplication {
 	private static Logger logger = LoggerFactory.getLogger(ServiceApplication.class);
 	public static void main(String[] args) {
