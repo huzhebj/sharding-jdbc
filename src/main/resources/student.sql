51c4018016fadbed53213817d82fa7e666b2ebc8
huzhe <huzhe@hulala.com>
2019-03-13 19:24:11 +0800

    修改sql

--- src/main/resources/student.sql
+++ src/main/resources/student.sql
@@ -1,6 +1,7 @@
 #创建学生表
 CREATE TABLE `tbl_student_0` (
-	`id` BIGINT(20) NOT NULL COMMENT '学生id',
+	`id` BIGINT(64) NOT NULL COMMENT '学生id',
+	`age` INT(3) NOT NULL DEFAULT '0' COMMENT '年龄',
 	`name` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '学生姓名',
 	PRIMARY KEY (`id`)
 )
@@ -9,7 +10,8 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
 
 CREATE TABLE `tbl_student_1` (
-	`id` BIGINT(20) NOT NULL COMMENT '学生id',
+	`id` BIGINT(64) NOT NULL COMMENT '学生id',
+	`age` INT(3) NOT NULL DEFAULT '0' COMMENT '年龄',
 	`name` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '学生姓名',
 	PRIMARY KEY (`id`)
 )
@@ -18,7 +20,8 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
 
 CREATE TABLE `tbl_student_2` (
-	`id` BIGINT(20) NOT NULL COMMENT '学生id',
+	`id` BIGINT(64) NOT NULL COMMENT '学生id',
+	`age` INT(3) NOT NULL DEFAULT '0' COMMENT '年龄',
 	`name` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '学生姓名',
 	PRIMARY KEY (`id`)
 )
@@ -27,7 +30,8 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
 
 CREATE TABLE `tbl_student_3` (
-	`id` BIGINT(20) NOT NULL COMMENT '学生id',
+	`id` BIGINT(64) NOT NULL COMMENT '学生id',
+	`age` INT(3) NOT NULL DEFAULT '0' COMMENT '年龄',
 	`name` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '学生姓名',
 	PRIMARY KEY (`id`)
 )
